46e481a65ff51573c49ca0b5c7e1dd834f6787ee
Raphael RALAIKOA <raph35ral@gmail.com>
2021-11-21 17:29:51 +0300

    Resolution bug dans le menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListActivitiesComponent } from './list-activities/list-activities.component';
+import { ListCardComponent } from './list-card/list-card.component';
 import { OverviewComponent } from './overview/overview.component';
+import { PayementComponent } from './payement/payement.component';
 
 const routes: Routes = [
   {
-    path: "home",
-    redirectTo: "",
-    pathMatch: "full"
+    path: "card",
+    component: ListCardComponent
+  },
+  {
+    path: 'payement',
+    component: PayementComponent,
+  },
+  {
+     path: "report",
+     component: ListActivitiesComponent
   },
   {
     path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  {
+    path: "home",
     component: OverviewComponent
   }
 ];
